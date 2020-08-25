549610546fc99933f6ee22dd9ea32bc0894da182
bortes <>
2020-08-25 04:17:33 -0300

    Alteração para modificar seleção de qual componente será exibido ao usuário quando este acessar a dApp.

--- app/src/components/Wadaag.js
+++ app/src/components/Wadaag.js
@@ -1,10 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 
-import { blockUI, unblockUI } from '../actions/BlockerAction';
-import NewUser from './NewUser';
 import OwnerUser from './OwnerUser';
-import WadaagService from '../services/WadaagService';
 
 /**
  * Componente responsavel por exibir a pagina principal.
@@ -12,36 +8,7 @@ import WadaagService from '../services/WadaagService';
  * @author bortes
  */
 function Wadaag(props) {
-    const [ready, setReady] = useState(false);
-    const [isOwner, setIsOwner] = useState(false);
-
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(blockUI('carregando dados...'));
-
-        WadaagService.isOwner()
-            .then(data => {
-                setIsOwner(data);
-            })
-            .finally(() => {
-                setReady(true)
-                dispatch(unblockUI());
-            });
-        }, []);
-
-    if (!ready) {
-        return (
-            <div>
-            </div>
-        );
-    }
-
-    return (
-        <div>
-            {isOwner ? <OwnerUser /> : <NewUser />}
-        </div>
-    );
+    return <OwnerUser />;
 }
 
 export default Wadaag;
